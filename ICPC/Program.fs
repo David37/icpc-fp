0453f37cb9f4386ecd3102e46ef7d41989571160
David37 <davidkondic@protonmail.com>
2020-05-21 23:57:27 +0200

    Worked on River, added functions. Functionality still incomplete

--- ICPC/Program.fs
+++ ICPC/Program.fs
@@ -159,23 +159,64 @@ let isValidRiver input = //Checks to see if the input given for river is valid
                 checkCharacters 0 charList
     | _ -> false
 
+let rec getWordLength i wordIndex wordList= //Gets the length of a specifc index given and its index in a list of strings
+        match i=wordIndex with
+        | true -> List.fold(fun acc item -> acc+1) 0 (Seq.toList((List.item i wordList)))
+        | _ -> getWordLength (i+1) wordIndex wordList
+
+let rec longestWord i listLength longestLength input= // Finds the length of the longest word given a list of strings
+    match i=listLength with
+    | true -> longestLength 
+    | _ -> longestWord (i+1) listLength (let curWordLength= getWordLength 0 i input
+                                         match longestLength < curWordLength with 
+                                         |true -> curWordLength 
+                                         | _ -> longestLength) input
+
+let rec newLineTextList index prevLength lineWidth lineText wordList lineTextList=
+    match index<>List.length wordList with
+    | true ->   let wordLength = getWordLength 0 index wordList
+                match prevLength = 0 with
+                | true -> match wordLength<lineWidth,wordLength=lineWidth with //first word on a new line
+                          | true,false ->newLineTextList (index+1) wordLength lineWidth (List.item index wordList) wordList lineTextList
+                          | false,true -> newLineTextList (index+1) wordLength lineWidth "" wordList (lineTextList@[(List.item index wordList)])
+                          | _ -> failwith "Error in calculating the longest word"
+                | _ -> let newLineLength = 1 + prevLength + wordLength
+                       let newLineText = lineText + " " + List.item index wordList
+                       match newLineLength < lineWidth, newLineLength = lineWidth with
+                       | true,false -> newLineTextList (index+1) newLineLength lineWidth newLineText wordList lineTextList // existing text fits into new line with additional space left
+                       | false,true -> newLineTextList (index+1) newLineLength lineWidth "" wordList (lineTextList@[newLineText]) // words existing text fits into new line with no additional space
+                       | false,false -> newLineTextList index 0 lineWidth "" wordList (lineTextList@[lineText]) // word doesn't fit has to go onto new line
+                       | _ -> failwith "Error has occured with the line width"
+    | _ -> lineTextList
 
 
-let rec testLineLength text i length  lineWordList=
-    let charList = Seq.toList(text)
-    match charList with
-    | [] -> false
-    | head::tail -> match head=' ' with//if charcater is space means it is a new word
-                    | true -> match i>=length with
-                              | true -> testLineLength text (i+1) length  (lineWordList)//cant add to current line
-                              | false -> testLineLength text (i+1) length  (lineWordList) //adds to current line
-                    | false -> testLineLength text (i+1) length  lineWordList//keep checking the length of the character
+let rec whitespaceIndices i xs out= // Returns a list of the indices in which there is a whitespace
+    match xs with
+    | [] -> out
+    | head::tail -> match head=' ' with
+                    | true -> whitespaceIndices (i+1) tail (out@[i])
+                    | _ -> whitespaceIndices (i+1) tail out
+
+//getLongestRiver still needs to be fully implemented to work
+let rec getLongestRiver index count xs out=
+    match xs with
+    | [] -> count
+    | head::next::tail -> let curLine= Seq.toList head
+                          let nextLine= Seq.toList next
+                          let indexes=0
+                          match (List.tryItem (indexes-1) nextLine)=Some ' ',
+                                (List.tryItem indexes nextLine)=Some ' ',
+                                (List.tryItem (indexes+1) nextLine)=Some ' ' with
+                          | true,_,_ -> indexes-1
+                          | _,true,_ -> indexes
+                          | _,_,true -> indexes+1
+                          | _ -> -1
+    | head::tail -> count
 
 let rivers input =
     match isValidRiver input with
     | true -> Some ""
     | false -> None
-
 
 [<EntryPoint>]
 let main argv =
